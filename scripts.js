dad85100df304e9daf786313bc348c9587b6dabf
Tamara Simões <tamara.backups@gmail.com>
2024-09-10 09:57:26 -0300

    Exemplos de demonstração

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Demonstração da aula
+
+function botao1() {
+    alert('Eu te disse para não clicar aqui!');
+}
+
+function botao2() {
+    const nome = document.getElementById("nome").value;
+    alert(`Olá, ${nome}! Boas-vindas ao mundo do JavaScript`)
+}

6ef5beadc9a069cc33067d3765cfa0b77db5eed9
Mateus Milanez <mateusmilanezdepaula@hotmail.com>
2024-09-10 11:24:33 -0300

    pronto

---
+++ ex.js
@@ -0,0 +1,25 @@
+let nome = "João";
+console.log(nome);
+
+nome = "Maria";
+console.log(nome);
+
+const idade = 30;
+console.log(idade);
+
+// idade = 10;
+// console.log(idade);
+
+// nao funciona o idade por conta q const é uma variavel que nao pode ser alterada.
+
+// nunmber
+let n1=10;
+console.log(typeof(n1));
+// typeof é uma função que retorna o tipo da variavel(numero)
+
+//string
+let feriado = "Natal";
+console.log(typeof(feriado));
+
+//boolean
+let chovendo = false;
---
+++ exer.js
@@ -0,0 +1,42 @@
+//exercicio 1
+let nome = "Mateus";
+let idade = 19;
+let cidade = "Belo horizonte";
+let pais = "Brasil";
+console.log(nome, idade, cidade, pais);
+
+//exercicio 2
+let as = 10;
+let bs = 20;
+
+console.log(as + bs);
+
+//exercicio 3
+let id = 19;
+if (id >= 18) {
+    console.log("Maior de idade");
+}
+else {
+    console.log("Menor de idade");
+}
+
+//exercicio 4
+let temCarteiradeMorotista= true;
+let temCarro = true;
+if (temCarteiradeMorotista && temCarro) {
+    console.log("Pode dirigir");
+}
+
+//exercicio 5
+let nota= 7;
+if (nota >= 7) {
+    console.log("Aprovado");
+}
+else if (5<=nota) {
+    console.log("Recuperação");
+}
+else {
+    console.log("Reprovado");
+}
+
+
